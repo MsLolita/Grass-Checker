a18d66bc614281edff51b67f53dbdf55db626c5f
Zeroknowledge Narrative <kostenko.dmi@yandex.com>
2024-10-08 22:30:29 +0300

    Fixed CSV

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-
 import asyncio
 import json
 import csv
@@ -8,7 +7,6 @@ from better_proxy import Proxy
 import aiohttp
 from tabulate import tabulate
 from fake_useragent import UserAgent
-
 
 from data.config import THREADS
 from utils import logger
@@ -59,7 +57,7 @@ class AirdropAllocator:
         self.masked_wallet = f"{self.wallet_address[:6]}...{self.wallet_address[-6:]}"
         self.proxy = proxy and Proxy.from_str(proxy).as_url
         self.index = index
-        self.base_url = 'https://api.getgrass.io/airdropAllocations'
+        self.base_url = 'https://api.getgrass.io/airdropAllocationsV2'
         self.headers = {
             'accept': 'application/json, text/plain, */*',
             'accept-language': 'en-US,en;q=0.9',
@@ -78,7 +76,7 @@ class AirdropAllocator:
         self.results_table = []
         self.table_formatter = table_formatter
 
-    @retry(wait=wait_exponential(min=2, max=7), stop=stop_after_attempt(7))
+    @retry(wait=wait_exponential(min=2, max=6), stop=stop_after_attempt(15))  # Увеличено количество попыток до 15
     async def fetch_airdrop_allocation(self):
         async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
             async with session.get(f'{self.base_url}?input=%7B%22walletAddress%22:%22{self.wallet_address}%22%7D',
@@ -105,12 +103,25 @@ class AirdropAllocator:
         if not result_data:
             return
 
-        with open(filename, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Tier', 'Amount'])
+        # Чтение существующего CSV файла и получение уже записанных адресов кошельков
+        existing_wallets = set()
+        try:
+            with open(filename, 'r') as file:
+                reader = csv.reader(file)
+                next(reader)  # Пропустить заголовок
+                for row in reader:
+                    existing_wallets.add(row[0])
+        except FileNotFoundError:
+            pass
 
-            for key, value in result_data.items():
-                writer.writerow([key, value])
+        # Если кошелек уже есть в CSV, пропускаем его
+        if self.wallet_address in existing_wallets:
+            return
+
+        # Дописываем данные в CSV файл
+        with open(filename, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([self.wallet_address, result_data.get('all', 0)])
 
     async def format_console_output(self, data, status):
         global all_tokens
@@ -140,6 +151,8 @@ class AirdropAllocator:
                         f.write(f"{self.wallet_address}\n")
                 else:
                     status = "Eligible"
+                    with open("logs/eligible.txt", "a") as f:
+                        f.write(f"{self.wallet_address}\n")
 
                 await self.format_console_output(totals, status)
                 self.beautify_and_log(data, log_filename)
@@ -147,14 +160,17 @@ class AirdropAllocator:
             except Exception as e:
                 await self.format_console_output({}, "Error")
 
+
 async def read_file_lines(file_path):
     with open(file_path, 'r') as file:
         return [line.strip() for line in file if line.strip()]
+
 
 async def print_table_headers():
     headers = ["#", "Wallet Address", "Total Tokens", "Status"]
     async with output_lock:
         print(tabulate([], headers=headers, tablefmt="grid"))
+
 
 async def main():
     path = "data"
@@ -179,6 +195,7 @@ async def main():
 
     print("+-----+------------------+----------------+----------+")
     logger.success(f"Total tokens: {all_tokens}")
+
 
 if __name__ == '__main__':
     all_tokens = 0
